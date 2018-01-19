072056e4626b85f1700d77e0cb95e0fd964196cf
Max Tyler <mtyler88@gmail.com>
2018-01-19 18:16:49 +0000

    Added an item type

--- src/parser.rs
+++ src/parser.rs
@@ -4,6 +4,7 @@ use types::{DateTime, Time};
 use nom::{be_u8, be_u32, digit, IResult};
 use std::str::{from_utf8, FromStr};
 
+/// Get a number from an array of u8
 named!(pub get_num<u32>, 
     map_res!(
         map_res!(digit,
@@ -13,6 +14,7 @@ named!(pub get_num<u32>,
     ) 
 );
 
+/// Get a time, from a string of hh:mm, colon-optional!
 named!(pub get_time<Time>, do_parse!(
         h: flat_map!(take!(2), get_num) >>
         opt!(tag!(":")) >>
@@ -20,6 +22,8 @@ named!(pub get_time<Time>, do_parse!(
         (Time{hours: h as u8, minutes: m as u8})
         ));
 
+/// Get a datetime struct from a string, dates separated by / or -, and optional time which can be
+/// separated by t/T
 named!(pub get_datetime<DateTime>, do_parse!(
         y: flat_map!(take!(4), get_num) >>
         one_of!("-/") >>
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,8 @@
 //! Types for the todo file parsing
 
+use std::rc::Rc;
+use std::cell::RefCell;
+
 /// Struct that holds time information, hours and optional minutes
 #[derive(Debug, PartialEq)]
 pub struct Time {
@@ -16,9 +19,31 @@ pub struct DateTime {
     pub time: Option<Time>,
 }
 
+/// Struct containing a todo item
+#[derive(Debug, PartialEq)]
+pub struct Item {
+    pub todo: Option<bool>,
+    pub text: String,
+    pub time: Option<DateTime>,
+    pub description: Option<String>,
+    pub children: Vec<Rc<RefCell<Item>>>,
+}
+
 impl Time {
     /// Find the difference between two times
     pub fn diff(&self, ref other: &Time) {
 
+    }
+}
+
+impl Item {
+    pub fn new_default() -> Item {
+        Item {
+            todo: None,
+            text: String::new(),
+            time: None,
+            description: None,
+            children: vec!(),
+        }
     }
 }
